528e031c2e50bc875fd6d688dc037f9f4cf3514a
Rapheal Chizitere <www.rashyking05@gmail.com>
2023-12-05 01:48:34 +0100

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,16 @@
+// import legacy from "@vitejs/plugin-legacy";
 import solid from "solid-start/vite";
+// import imagemin from "unplugin-imagemin/vite";
 import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid({
+      ssr: true,
+    }),
+    // legacy({
+    //   targets: ["defaults", "not IE 11"],
+    // }),
+    // imagemin(),
+  ],
 });
